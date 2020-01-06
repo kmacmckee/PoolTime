6c290b81b03a334fdfa8b4473475148f3d3b09a0
Kobe McKee <kobe.mckee@gmail.com>
2020-01-06 15:48:03 -0700

    Set up LandingVC and Register1VC

---
+++ PoolTime/View Controllers/LandingViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LandingViewController.swift
+//  PoolTime
+//
+//  Created by Kobe McKee on 1/6/20.
+//  Copyright © 2020 Kobe McKee. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    @IBAction func nextButtonPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "toRegister1", sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PoolTime/View Controllers/Register1ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  Register1ViewController.swift
+//  PoolTime
+//
+//  Created by Kobe McKee on 1/6/20.
+//  Copyright © 2020 Kobe McKee. All rights reserved.
+//
+
+import UIKit
+
+class Register1ViewController: UIViewController {
+
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func nextButtonPressed(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
