0df78502a09d445aff8fee3cff71217c2a3a2fe3
Kobe McKee <kobe.mckee@gmail.com>
2020-01-06 14:39:17 -0700

    Initial Commit

---
+++ PoolTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PoolTime
+//
+//  Created by Kobe McKee on 1/6/20.
+//  Copyright © 2020 Kobe McKee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
