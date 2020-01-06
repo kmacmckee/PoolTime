25a2fe9aa49e86baf58a571a2903b2492074135b
Kobe McKee <kobe.mckee@gmail.com>
2020-01-06 15:59:49 -0700

    Created Register2VC and UserController to handle onboarding/log in process

---
+++ PoolTime/UserController.swift
@@ -0,0 +1,30 @@
+//
+//  UserController.swift
+//  PoolTime
+//
+//  Created by Kobe McKee on 1/6/20.
+//  Copyright © 2020 Kobe McKee. All rights reserved.
+//
+
+import Foundation
+
+class UserController {
+    
+    
+    
+    func checkIfRegistered() {
+        // check if the number inputted has an account already
+    }
+    
+    func logInUser() {
+        // use inputted number to log in the user
+    }
+    
+    func registerUser() {
+        // use inputted info to create a new user account
+    }
+    
+    
+    
+    
+}
---
+++ PoolTime/View Controllers/Register2ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  Register2ViewController.swift
+//  PoolTime
+//
+//  Created by Kobe McKee on 1/6/20.
+//  Copyright © 2020 Kobe McKee. All rights reserved.
+//
+
+import UIKit
+
+class Register2ViewController: UIViewController {
+
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confrimPasswordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
